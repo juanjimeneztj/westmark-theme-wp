5fd656a377bb0bf81c4bf48c667c15c6669f7a56
Juan Jimenez <juanjimeneztj@gmail.com>
2021-02-21 07:44:16 -0800

    updated guide images

--- guide.php
+++ guide.php
@@ -10,8 +10,8 @@ require_once(ABSPATH . 'wp-admin/admin.php');
 require_once(ABSPATH . 'wp-admin/admin-header.php');
 ?>
 <link rel="stylesheet" href="<?=get_site_url()?>/wp-content/themes/american_founders/css/bootstrap.min.css">
-<link rel="stylesheet" href="<?=get_site_url()?>/wp-content/themes/american_founders/css/dashboard.css">
 <link rel="stylesheet" href="<?=get_site_url()?>/wp-content/themes/american_founders/css/theme.css">
+<link rel="stylesheet" href="<?=get_site_url()?>/wp-content/themes/american_founders/css/dashboard.css">
 
 <section id="gsweb-dashboard" class="p-4">
 	<div class="container-fluid">
@@ -29,8 +29,8 @@ require_once(ABSPATH . 'wp-admin/admin-header.php');
 </section>
 <section id="guide" class="p-4">
 	<div class="container-fluid">
-	<div class="row">
-			<div class="col">
+		<div class="row align-items-center justify-content-center">
+			<div class="col-12 col-md-8">
 				<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
 					<li class="nav-item">
 						<a class="nav-link active" id="pills-create-post-tab" data-toggle="pill" href="#pills-create-post" role="tab" aria-controls="pills-create-post" aria-selected="true">How to create a post?</a>
@@ -42,50 +42,52 @@ require_once(ABSPATH . 'wp-admin/admin-header.php');
 						<a class="nav-link" id="pills-gsweb-theme-tab" data-toggle="pill" href="#pills-gsweb-theme" role="tab" aria-controls="pills-gsweb-theme" aria-selected="false">Modify theme appearance</a>
 					</li>
 				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col">
 				<div class="tab-content" id="pills-tabContent">
 					<div class="tab-pane fade show active" id="pills-create-post" role="tabpanel" aria-labelledby="pills-create-post-tab">
-						<h5>To generate a new post, you can create it directly from the main dashboard as shown in the following image:</h5>
+						<p class="pt-4">To generate a new post, you can create it directly from the main dashboard as shown in the following image:</p>
 
-						<figure class="py-3"><img class="img-fluid" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0001.png" alt=""></figure>
+						<figure class="text-center"><img class="img-fluid w-100" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0001.gif" alt=""></figure>
 
-						<h5>or you can go directly to the posts section by clicking on the left sidebar on the POSTS option, in this section you can do it in two different options which you can find both in the upper part and in the sidebar on the left.</h5>
-						
-						<figure class="py-3"><img class="img-fluid" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0002.png" alt=""></figure>
-						
+						<p>or you can go directly to the posts section by clicking on the left sidebar on the POSTS option, in this section you can do it in two different options which you can find both in the upper part and in the sidebar on the left.</p>
+
 						<div class="row align-items-center justify-content-center">
 							<div class="col-12 col-md-6">
-								<h1 class="text-center">On the next screen is where you can enter both the title of the post and its content</h1>
+								<p class="text-center"><strong>Option 1</strong></p>
+								<figure class="text-center"><img class="img-fluid w-100" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0002.gif" alt=""></figure>
 							</div>
 							<div class="col-12 col-md-6">
-								<figure class="py-3"><img class="img-fluid" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0003.png" alt=""></figure>
+								<p class="text-center"><strong>Option 2</strong></p>
+								<figure class="text-center"><img class="img-fluid w-100" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0002b.gif" alt=""></figure>
 							</div>
 						</div>
+						
+						<p>On the next screen is where you can enter both the title of the post and its content</p>
+						
+						<figure class="text-center"><img class="img-fluid w-100" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0003.png" alt=""></figure>
 
-						<div class="row align-items-center justify-content-center">
-							<div class="col-12 col-md-9">
-								<figure class="py-3"><img class="img-fluid" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0004.png" alt=""></figure>
-							</div>
-							<div class="col-12 col-md-3">
-								<h3>On this same screen you can find a section called categories, where you can choose the section to which the post belongs</h3>
-							</div>
-						</div>
-						<div class="row align-items-center justify-content-center">
-							<div class="col-12 col-md-3">
-								<h3>There is also a section where you will have to add the main image of the post</h3>
-							</div>
-							<div class="col-12 col-md-9">
-								<figure class="py-3"><img class="img-fluid" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0005.png" alt=""></figure>
-							</div>
-						</div>
+						<p>On this same screen you can find a section called categories, where you can choose the section to which the post belongs</p>
+
+						<figure class="text-center"><img class="img-fluid w-100" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0004.gif" alt=""></figure>
+						
+						<p>There is also a section where you will have to add the main image of the post</p>
+						
+						<figure class="text-center"><img class="img-fluid w-100" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0005.gif" alt=""></figure>
+						
 						<div class="row align-items-center justify-content-center">
 							<div class="col-12">
-								<h3>Last but not least, it is the option that allows users to add comments to the post, but these will not be reflected unless the site administrator approves them in advance.</h3>
+								<p>Last but not least, it is the option that allows users to add comments to the post, but these will not be reflected unless the site administrator approves them in advance.</p>
 							</div>
 							<div class="col-12 col-md-6">
-								<figure class="py-3"><img class="img-fluid" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0006.png" alt=""></figure>
+								<figure class="text-center"><img class="img-fluid" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0006.png" alt=""></figure>
 							</div>
 							<div class="col-12 col-md-6">
-								<figure class="py-3"><img class="img-fluid" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0007.png" alt=""></figure>
+								<figure class="text-center"><img class="img-fluid" src="<?=get_site_url()?>/wp-content/themes/american_founders/images/guide/create_post_0007.png" alt=""></figure>
 							</div>
 						</div>
 					</div>
